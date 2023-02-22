74086067a2cf5f1375aee7a01937faf43fe04b89
OSiG43 <simon.galand43@gmail.com>
2023-02-22 19:34:45 +0100

    Premier commit avec fonction main

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def hello():
+    print("hello world")
+
+hello()
